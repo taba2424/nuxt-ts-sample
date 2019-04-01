322b59799fb2d59dd33d000fa4d3dbffcc68d95a
nishikawatadashi <a151417k@st.u-gakugei.ac.jp>
2019-04-01 19:43:08 +0900

    sample vue ファイルの作成

---
+++ app/pages/hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Hello {{ message }}!!</h1>
+
+    <input type="text" v-model="message">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+
+export default class extends Vue {
+  /* data */
+  message: string = 'World'
+}
+</script>
---
+++ app/pages/point.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>X: {{ halfPoint.x }}</p>
+    <p>Y: {{ halfPoint.y }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+
+export default class extends Vue {
+  /* data */
+  width: number = 800
+  height: number = 600
+
+  /* computed */
+  get halfWidth(): number {
+    return this.width / 2
+  }
+
+  get halfHeight(): number {
+    return this.height / 2
+  }
+
+  get halfPoint(): { x: number, y: number } {
+    return {
+      x: this.halfWidth,
+      y: this.halfHeight
+    }
+  }
+}
+</script>
